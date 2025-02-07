30845202e24220e2d4f5b98859f926fad442d461
leap0489 <144965129+leap0489@users.noreply.github.com>
2025-02-07 01:05:50 +0000

    add food options

--- reply.js
+++ reply.js
@@ -1,9 +1,29 @@
 export function reply(message){
-    let reply = `${message} to you too`;
-    if(message.toLowerCase().includes("nice")){
-        reply = "Yes ... I think that's nice too"
-    
+    let reply = `Thanks for choosing to order with us. Would you like a pizza or chicken tenders? `;
+    if(message.toLowerCase().includes("pizza")){
+        reply = "What size would you like? We have small, medium, large."
     }
+    else if(message.toLowerCase().includes("small") || message.toLowerCase().includes("medium") || message.toLowerCase().includes("large")){
+        reply = "Now what toppings would you like. We have cheese or pepperoni."
+    }
+    else if (message.toLowerCase().includes("cheese")){
+        reply = "One cheese pizza on the way to you! Payment will be required upon delivery."
+    }
+    else if (message.toLowerCase().includes("pepperoni")){
+        reply = "One pepperoni pizza on the way to you! Payment will be required upon delivery."
+    }
+    else if (message.toLowerCase().includes("chicken") || message.toLowerCase().includes("tenders")){
+        reply = "Awesome! How many pieces would you like? We can do 3 or 5 pieces."
+    }
+    else if (message.toLowerCase().includes("3") || message.toLowerCase().includes("5")){
+        reply = "Great! Do you want plum or ranch as your dipping sauce?"
+    }
+    else if (message.toLowerCase().includes("plum") || message.toLowerCase().includes("ranch")){
+        reply = "One chicken strip order on the way to you! Payment will be required upon delivery."
+    }
+
+
+
 
     return reply;
 }
